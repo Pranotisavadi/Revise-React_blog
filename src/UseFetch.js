78904618f3f9e7281fd1ba7996be0212c3d69218
Pranoti Savadi <pranoti.savadi@gmail.com>
2022-04-10 21:31:59 -0400

    Cleanup and abort added

--- src/UseFetch.js
+++ src/UseFetch.js
@@ -8,8 +8,10 @@ const useFetch = (url) => {
    
    
     useEffect(() => {
+        const abortCont = new AbortController();
+
         setTimeout(() => {
-            fetch(url)
+            fetch(url, { signal: abortCont.signal })
         .then(res => {
             if(!res.ok) {
                 throw Error('could not fetch data for that resource');
@@ -23,10 +25,16 @@ const useFetch = (url) => {
             setError(null)
         })
         .catch(err => {
-            setIsPending(false);
-            setError(err.message);
+            if(err.name === 'AbortError'){
+                console.log("Abort error fetched")
+            }else{
+                setIsPending(false);
+                setError(err.message);
+            }
+            
         })
         }, 1000)
+        return() => abortCont.abort();
       }, [url])
       return {data, isPending, error}
 }
