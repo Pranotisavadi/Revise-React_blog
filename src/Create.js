8d6c099bcbf50d59f0ed0fe3d0bc2b786fb94400
Pranoti Savadi <pranoti.savadi@gmail.com>
2022-04-13 21:49:34 -0400

    controlled input

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,43 @@
+import { useState } from "react";
+
 const  Create = () => {
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const [author, setAuthor] = useState('Yoshi')
     
 
     return (
         <div className="create">
-        <h2>Create New Blog</h2>
+        <form>
+          <label>Blog Title:</label>
+          <input 
+               type="text"
+                reuired
+                value ={title}
+               onChange ={(e) => setTitle(e.target.value)} />
+          
+          <label>Blog Body:</label>
+          <textarea
+             reuired
+               value ={ body}
+               onChange ={(e) => setBody(e.target.value)} />
+         
+          <label>Blog Author:</label>
+          <select 
+          value = { author }
+          onChange = {(e) => setAuthor(e.target.value)}
+          >
+            <option value="Mario">Mario</option> 
+            <option value="Yoshi">Yoshi</option>
+          </select>
+          <button>Add Blog</button>
+             </form>
+             <div>
+             <p>{ title }</p>
+             <p> { body}</p>
+             <p>{ author }</p>
+             </div>
+            
         </div>
         
       );
